5a98fc85a3eaa5e33c65aafe196d58480fbfc960
Henning Jensen <henning@arkitektum.no>
2019-02-21 16:04:29 +0100

    Initial version

---
+++ src/Geonorge.AuthLib.Common/BaatAuthzApi.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Geonorge.AuthLib.Common.Logging;
+
+namespace Geonorge.AuthLib.Common
+{
+    public interface IBaatAuthzApi
+    {
+        /// <summary>
+        /// returns information about a user, e.g. name and organization
+        /// </summary>
+        /// <param name="username"></param>
+        Task<BaatAuthzUserInfoResponse> Info(string username);
+    }
+
+    /// <summary>
+    /// Implementation of the BaatAuthzApi. Returns authorization information about BAAT users. 
+    /// </summary>
+    public class BaatAuthzApi : IBaatAuthzApi
+    {
+        private static readonly ILog Log = LogProvider.For<BaatAuthzApi>(); 
+
+        private readonly string _apiUrl;
+        private readonly string _apiCredentials;
+        private readonly IHttpClientFactory _httpClientFactory;
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="apiUrl">the full url to the BAAT api endpoint</param>
+        /// <param name="apiCredentials">Http basic authentication params e.g. username:password </param>
+        /// <param name="httpClientFactory">provider of http clients used for communicating with BAAT</param>
+        public BaatAuthzApi(string apiUrl, string apiCredentials, IHttpClientFactory httpClientFactory)
+        {
+            _apiUrl = apiUrl;
+            _apiCredentials = apiCredentials;
+            _httpClientFactory = httpClientFactory;
+        }
+        
+        /// <summary>
+        /// Returns information about a user, e.g. name and organization
+        /// </summary>
+        /// <param name="username"></param>
+        public async Task<BaatAuthzUserInfoResponse> Info(string username)
+        {
+            var url = $"{_apiUrl}authzinfo/{username}";
+            Log.Debug("Fetching data from {url}", url);
+            
+            var res = await GetClient().GetAsync(url);
+
+            if (!res.IsSuccessStatusCode)
+            {
+                Log.Error("Looking up {user} from BaatAuthzApi failed with status code: {code}", username, res.StatusCode);
+                return BaatAuthzUserInfoResponse.Empty;
+            }
+            
+            var json = await res.Content.ReadAsStringAsync();
+            
+            Log.Debug("Response from BaatAuthzApi: {json}", json);
+            
+            return JsonConvert.DeserializeObject<BaatAuthzUserInfoResponse>(json);
+        
+        }
+
+        private HttpClient GetClient()
+        {
+            var client = _httpClientFactory.CreateClient();
+            
+            Log.Debug("Connecting to BaatAuthzApi with credentials: {credentials}", _apiCredentials);
+            
+            var byteArray = Encoding.ASCII.GetBytes(_apiCredentials);
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+            return client;
+        }
+
+    }
+}
---
+++ src/Geonorge.AuthLib.Common/GeonorgeAuthorizationService.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Geonorge.AuthLib.Common.Logging;
+
+namespace Geonorge.AuthLib.Common
+{
+    /// <summary>
+    /// Retrieves information from Geonorges authorization service, also known as BAAT.
+    /// </summary>
+    public class GeonorgeAuthorizationService : IGeonorgeAuthorizationService
+    {
+        private static readonly ILog Log = LogProvider.For<GeonorgeAuthorizationService>(); 
+
+        private readonly IBaatAuthzApi _baatAuthzApi;
+
+        public GeonorgeAuthorizationService(IBaatAuthzApi baatAuthzApi)
+        {
+            _baatAuthzApi = baatAuthzApi;
+        }
+
+        /// <summary>
+        ///     Returning claims for the given user from BaatAuthzApi.
+        /// </summary>
+        /// <param name="identity"></param>
+        /// <returns></returns>
+        public async Task<List<Claim>> GetClaims(ClaimsIdentity identity)
+        {
+            Claim usernameClaim =
+                identity.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier");
+
+            BaatAuthzUserInfoResponse response = await _baatAuthzApi.Info(usernameClaim.Value);
+
+            var claims = new List<Claim>();
+
+            if (response == BaatAuthzUserInfoResponse.Empty)
+                Log.Warn("Empty response from BaatAuthzApi - no claims appended to user");
+            else
+                claims.AddRange(new List<Claim>
+                {
+                    new Claim("Name", string.IsNullOrEmpty(response?.Name) ? "" : response.Name),
+                    new Claim("Email", response.Email),
+                    new Claim("AuthorizedFrom", response.AuthorizedFrom),
+                    new Claim("AuthorizedUntil", response.AuthorizedUntil),
+                    new Claim("OrganizationName", response.Organization?.Name),
+                    new Claim("OrganizationOrgnr", response.Organization?.Orgnr),
+                    new Claim("OrganizationContactName", response.Organization?.ContactName),
+                    new Claim("OrganizationContactEmail", response.Organization?.ContactEmail),
+                    new Claim("OrganizationContactPhone", response.Organization?.ContactPhone)
+                });
+
+            return claims;
+        }
+    }
+}
