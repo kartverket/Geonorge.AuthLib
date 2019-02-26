1c05111ad4d9ea33b8f2f638200c162a56a21403
Henning Jensen <henning@arkitektum.no>
2019-02-26 22:51:18 +0100

    Add Owin middleware for configuring authentication

---
+++ src/Geonorge.AuthLib.NetFull/GeonorgeAuthenticationMiddleware.cs
@@ -0,0 +1,77 @@
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Autofac;
+using Autofac.Integration.Owin;
+using Geonorge.AuthLib.Common;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Owin;
+
+namespace Geonorge.AuthLib.NetFull
+{
+    public static class GeonorgeAuthenticationMiddleware
+    {
+        /// <summary>
+        /// Configure Geonorge authentication with GeoID and BaatAuthzApi.
+        /// Required AppSettings:
+        /// * GeoID:ClientId
+        /// * GeoID:ClientSecret
+        /// * GeoID:MetadataAddress
+        /// * GeoID:Authority
+        /// * GeoID:RedirectUri
+        /// * GeoID:Issuer
+        /// * GeoID:BaatAuthzApiUrl
+        /// * GeoID:BaatAuthzApiCredentials
+        /// </summary>
+        /// <param name="app"></param>
+        public static void UseGeonorgeAuthentication(this IAppBuilder app)
+        {
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+            
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+            app.UseOpenIdConnectAuthentication(
+                new OpenIdConnectAuthenticationOptions
+                {
+                    ClientId = GetAppSetting("GeoID:ClientId"),
+                    ClientSecret = GetAppSetting("GeoID:ClientSecret"),
+                    MetadataAddress = GetAppSetting("GeoID:MetadataAddress"),
+                    Authority = GetAppSetting("GeoID:Authority"),
+                    RedirectUri = GetAppSetting("GeoID:RedirectUri"),
+                    PostLogoutRedirectUri = GetAppSetting("GeoID:RedirectUri"),
+                    Scope = OpenIdConnectScope.OpenId,
+                    ResponseType = OpenIdConnectResponseType.CodeIdTokenToken,
+                    TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidIssuer = GetAppSetting("GeoID:Issuer")
+                    },
+                    Notifications = new OpenIdConnectAuthenticationNotifications()
+                    {
+                        SecurityTokenValidated = async (context) =>
+                        {
+                            context.AuthenticationTicket.Identity.AddClaim(new Claim("id_token",context.ProtocolMessage.IdToken));
+                            context.AuthenticationTicket.Identity.AddClaim(new Claim("access_token",context.ProtocolMessage.AccessToken));
+
+                            var geonorgeAuthorizationService = context.OwinContext.GetAutofacLifetimeScope().Resolve<IGeonorgeAuthorizationService>();
+                            context.AuthenticationTicket.Identity.AddClaims(await geonorgeAuthorizationService.GetClaims(context.AuthenticationTicket.Identity));
+                        },
+                        AuthenticationFailed = (context) =>
+                        {
+                            context.HandleResponse();
+                            context.Response.Redirect("/?errormessage=" + context.Exception.Message);
+                            return Task.FromResult(0);
+                        },
+                    }
+                }
+            );
+            
+        }
+        
+        private static string GetAppSetting(string paramName)
+        {
+            return System.Configuration.ConfigurationManager.AppSettings[paramName];
+        }
+    }
+}
