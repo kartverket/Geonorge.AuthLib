6a711d9580be1c1ce56ba3e84f3c0b8361472b1d
Henning Jensen <henning@arkitektum.no>
2019-02-28 13:43:28 +0100

    Add constants for user roles.

---
+++ src/Geonorge.AuthLib.Common/GeonorgeRoles.cs
@@ -0,0 +1,10 @@
+namespace Geonorge.AuthLib.Common
+{
+    public class GeonorgeRoles
+    {
+        public const string MetadataAdmin = "nd.metadata_admin";
+        public const string MetadataEditor = "nd.metadata_editor";
+        public const string DokAdmin = "nd.dok_admin";
+        public const string DokEditor = "nd.dok_editor";
+    }
+}
